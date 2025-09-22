34f1c376372d67ccc008a6df65c9bb760a117df2
stephen88888888 <56657466shen@gmail.com>
2025-09-22 17:45:05 +0800

    最近更改9,22 1744

---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+pub  mob handler;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
-use clap::Parser;
-use std::error;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(version, about = "Movie app", long_about = "Movie information app")]
-struct Cli {}
-
-fn main() {
-    let cli = Cli::parse();
+#[command(
+    version,
+    about= "Movie app",
+    long_about="Movie information app"
+)]
+struct Cli{
+ #[command(subcommand)]
+ commands: Option<Commands>
 }
-//j;lkj;j;klj;j
+
+#[derive(Subcommand)]
+enum Commands{
+    ///user log into the system
+    Login{
+        ///The username of the user
+        #[arg(short,long)]
+        username:String
+    }
+}
+
+fn main(){
+    let cli=Cli::parse();
+    match &cli.commands{
+        Some(Commands::Login{username})=>{}
+        _=>println!("No command provided or command nto recognized"),
+
+    }
+
+    Ok(())
+   
+}
